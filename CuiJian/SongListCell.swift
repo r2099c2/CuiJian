e1b0c35c6a0c3671e8082ba13e16e53f8af1bdf4
Rick li <285699093@qq.com>
2016-02-18 16:34:20 +0800

    2-18

---
+++ CuiJian/SongListCell.swift
@@ -0,0 +1,15 @@
+//
+//  SongListCell.swift
+//  CuiJian
+//
+//  Created by Rick on 16/2/18.
+//  Copyright © 2016年 Rick. All rights reserved.
+//
+
+import UIKit
+
+class SongListCell: UITableViewCell {
+
+    @IBOutlet weak var songListIndex: UILabel!
+    @IBOutlet weak var songName: UILabel!
+}
--- CuiJian/SongListViewController.swift
+++ CuiJian/SongListViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class SongListViewController: UIViewController {
+class SongListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    let songList = ["光冻","死不回头","鱼鸟之恋","外面的妞","酷瓜树","金色早晨","滚动的蛋","浑水湖漫步","阳光下的梦"]
+   
+    @IBOutlet weak var songListTable: UITableView! {
+        didSet {
+            songListTable.delegate = self
+            songListTable.dataSource = self
+        }
+    }
     
     @IBAction func dismiss(sender: UIBarButtonItem) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    //MARK: - TableView
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let identifier = "songListCellIdentifier"
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as! SongListCell
+        
+        cell.songListIndex.text = String(indexPath.row)
+        cell.songName.text = songList[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songList.count
+    }
 }
